539361c9be7fb62b13672c70cb4c7ac6e6cc4353
sophiakc <sophiakc@gmail.com>
2016-12-03 15:10:09 -0800

    proceed w pod install

---
+++ Pods/Target Support Files/Pods-CalendarCollectionViewUITests/Pods-CalendarCollectionViewUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CalendarCollectionViewUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CalendarCollectionViewUITests
+@end
---
+++ Pods/Target Support Files/Pods-CalendarCollectionViewUITests/Pods-CalendarCollectionViewUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CalendarCollectionViewUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CalendarCollectionViewUITestsVersionString[];
+
