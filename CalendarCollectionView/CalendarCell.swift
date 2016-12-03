d57c3926fc4ccfe6642fc05d4914d385d12d78b8
sophiakc <sophiakc@gmail.com>
2016-12-03 14:35:27 -0800

    calendar collection view init from the shared screen w Charlie

---
+++ CalendarCollectionView/CalendarCell.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarCell.swift
+//  CalendarCollectionView
+//
+//  Created by Sophia KC on 12/1/16.
+//  Copyright © 2016 Sophia KC. All rights reserved.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell {
+
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var wordCountView: UIView!
+    @IBOutlet weak var entriesCountView: UIView!
+
+
+    
+    
+}
